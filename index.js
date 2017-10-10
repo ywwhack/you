b05b87810e811af8d85e4a00234cedc096ba40e2
ywwhack <yww7289@163.com>
2017-10-10 12:28:17 +0800

    re-structure dirs

--- index.js
+++ index.js
@@ -1,60 +1,7 @@
-let activeUpdate = null
-
-class Dep {
-  constructor () {
-    this.subscribers = new Set()
-  }
-  depend () {
-    if (activeUpdate) {
-      this.subscribers.add(activeUpdate)
-    }
-  }
-  notify () {
-    this.subscribers.forEach(sub => sub())
-  }
-}
-
-function observe (obj) {
-  const dep = new Dep()
-  Object.keys(obj).forEach(key => {
-    let internalValue = obj[key]
-    Object.defineProperty(obj, key, {
-      get () {
-        dep.depend()
-        return internalValue
-      },
-      set (newValue) {
-        internalValue = newValue
-        dep.notify()
-      }
-    })
-  })
-}
-
-function autoRun (update) {
-  function wrapperUpdate () {
-    activeUpdate = update
-    update()
-    activeUpdate = null
-  }
-  wrapperUpdate()
-}
+import { observe, autoRun, compile } from './lib'
 
 const obj = { message: 'world' }
 observe(obj)
-
-const handlebars = require('handlebars/dist/handlebars.js')
-const originalCompile = handlebars.compile.bind(handlebars)
-function compile (source) {
-  const template = originalCompile(source)
-  return function reactivityTemplate (data, update) {
-    let result
-    autoRun(() => {
-      result = template(data)
-      update(result)
-    })
-  }
-}
 
 const source = '<p>Hello, {{ message }}</p>'
 
